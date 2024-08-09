167a0a7c1c95efc124d5dc0fc88c692ff8212f31
Majed1945 <majedbiazid@gmail.com>
2024-08-09 23:27:47 +0300

    Footer

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -8,48 +8,89 @@ const MemberCard = ({ name, photo, link }) => {
   const theme = useTheme();
 
   return (
-    <Grid item xs={12} semiMd={6} lg={3} xxl={3}>
+    <Grid item xs={12} sm={6} xl={3}>
       <Stack
-        direction={"column"}
         sx={{
-          backgroundColor: theme.palette.primary.dark,
-          borderRadius: "15px",
           width: "100%",
+          borderRadius: "15px",
+          overflow: "hidden",
         }}
-        alignItems={"center"}
-        padding={"1rem"}
-        gap={"1rem"}
       >
-        <Stack sx={{ height: "260px", width: "260px" }}>
-          <img
-            src={photo}
-            alt={name}
-            style={{
-              height: "100%",
-              width: "100%",
-              borderRadius: "50%",
-              objectFit: "fill",
-            }}
-          />
-        </Stack>
+        {/* First Box */}
         <Stack
-          direction={"row"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          textAlign={"center"}
-          gap={"0.5rem"}
+          sx={{
+            backgroundColor: theme.palette.primary.dark,
+            height: "150px",
+            position: "relative",
+          }}
         >
-          <Typography variant="h5" color={"tertiary.main"}>
+          {/* Image Overlapping both boxes */}
+          <Stack
+            sx={{
+              position: "absolute",
+              left: "50%",
+              top: "120%",
+              transform: "translate(-50%, -50%)",
+              width: "80%",
+              border: `2px solid ${theme.palette.tertiary.main}`,
+              borderRadius: "10px",
+            }}
+          >
+            <img
+              src={photo}
+              alt={name}
+              style={{
+                width: "100%",
+                height: "280px",
+                objectFit: "cover", // Ensure image fills the container while maintaining aspect ratio
+                borderRadius: "10px",
+              }}
+            />
+          </Stack>
+        </Stack>
+
+        {/* Second Box */}
+        <Stack
+          sx={{
+            backgroundColor: theme.palette.primary.main,
+            height: "340px",
+            paddingTop: "130px", // To account for the image overlap
+            paddingLeft: "1rem",
+            paddingRight: "1rem",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography
+            variant="h5"
+            color={theme.palette.tertiary.main}
+            sx={{
+              transition: "color 0.3s ease",
+              "&:hover": {
+                color: theme.palette.secondary.main,
+              },
+            }}
+          >
             {name}
           </Typography>
           <FaLinkedin
             size={28}
             color={theme.palette.tertiary.main}
-            style={{ cursor: "pointer" }}
-            onClick={() => window.open(link)}
+            style={{ cursor: "pointer", marginTop: "0.5rem" }}
+            onClick={() => window.open(link, "_blank")}
+            className="linkedin-icon"
           />
         </Stack>
       </Stack>
+
+      {/* Inline styles for hover effect on the LinkedIn icon */}
+      <style jsx>{`
+        .linkedin-icon:hover {
+          color: ${theme.palette.secondary.main};
+          transform: scale(1.1);
+          transition: transform 0.3s ease, color 0.3s ease;
+        }
+      `}</style>
     </Grid>
   );
 };
--- src/shared/template/Footer/Footer.js
+++ src/shared/template/Footer/Footer.js
@@ -1,7 +1,193 @@
+import {
+  Stack,
+  Typography,
+  IconButton,
+  Box,
+  useMediaQuery,
+} from "@mui/material";
 import React from "react";
+import { useTheme } from "@emotion/react";
+import logoTransparent from "../../assets/images/logoTransparent.png";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+const sections = [
+  { id: "home", name: "Home" },
+  { id: "about-us", name: "About Us" },
+  { id: "vision-mission", name: "Vision and Mission" },
+  { id: "values", name: "Values" },
+  { id: "goals", name: "Goals" },
+  { id: "services", name: "Services" },
+  { id: "team", name: "Team" },
+];
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  const handleScroll = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      const offset = window.innerHeight * 0.1; // 10% of the viewport height
+      const elementPosition =
+        element.getBoundingClientRect().top + window.scrollY - 150;
+      const offsetPosition = elementPosition - offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <Stack
+      component="footer"
+      sx={{
+        backgroundColor: theme.palette.green.main,
+        color: theme.palette.common.white,
+        padding: "2rem",
+        flexWrap: "wrap", // Ensures wrapping on small screens
+      }}
+      spacing={4}
+    >
+      <Stack
+        direction={{ xs: "column", md: "row", width: "90%" }}
+        justifyContent="space-between"
+        alignItems="center"
+        spacing={4}
+        sx={{ flexWrap: "wrap" }} // Wrap items on small screens
+      >
+        {isMobile && (
+          <Box
+            sx={{
+              height: "100px",
+              "&:hover": { cursor: "pointer" }, // Change cursor on hover
+              marginTop: { xs: "1rem", sm: "0" }, // Add margin for better spacing on mobile
+              display: "flex",
+              justifyContent: "center", // Center the logo on mobile
+            }}
+            onClick={() => handleScroll("home")} // Navigate to "Home" on click
+          >
+            <img
+              src={logoTransparent}
+              alt="logo"
+              style={{
+                objectFit: "fill",
+                width: "100%",
+                height: "100%",
+              }}
+            />
+          </Box>
+        )}{" "}
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{
+            flexWrap: "wrap", // Wrap items in this section
+            justifyContent: { xs: "center", sm: "flex-start" }, // Center on mobile
+          }}
+        >
+          {sections.map((section) => (
+            <Typography
+              key={section.id}
+              onClick={() => handleScroll(section.id)}
+              sx={{
+                cursor: "pointer",
+                color: theme.palette.common.white,
+                "&:hover": {
+                  color: theme.palette.primary.dark,
+                  transition: "color 0.3s ease",
+                },
+                textAlign: { xs: "center", sm: "left" }, // Center text on mobile
+                marginBottom: { xs: "0.5rem", sm: "0" }, // Add margin for better spacing on mobile
+              }}
+              variant="body1"
+            >
+              {section.name}
+            </Typography>
+          ))}
+        </Stack>
+        {!isMobile && (
+          <Box
+            sx={{
+              height: "100px",
+              "&:hover": { cursor: "pointer" }, // Change cursor on hover
+              marginTop: { xs: "1rem", sm: "0" }, // Add margin for better spacing on mobile
+              display: "flex",
+              justifyContent: "center", // Center the logo on mobile
+            }}
+            onClick={() => handleScroll("home")} // Navigate to "Home" on click
+          >
+            <img
+              src={logoTransparent}
+              alt="logo"
+              style={{
+                objectFit: "fill",
+                width: "100%",
+                height: "100%",
+              }}
+            />
+          </Box>
+        )}
+      </Stack>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        sx={{ flexWrap: "wrap" }} // Wrap items on small screens
+      >
+        <IconButton
+          sx={{
+            color: theme.palette.common.white,
+            "&:hover": {
+              color: theme.palette.primary.light,
+              transition: "color 0.3s ease",
+            },
+          }}
+          href=""
+        >
+          <InstagramIcon />
+        </IconButton>
+        <IconButton
+          sx={{
+            color: theme.palette.common.white,
+            "&:hover": {
+              color: theme.palette.primary.light,
+              transition: "color 0.3s ease",
+            },
+          }}
+          href=""
+        >
+          <TwitterIcon />
+        </IconButton>
+        <IconButton
+          sx={{
+            color: theme.palette.common.white,
+            "&:hover": {
+              color: theme.palette.primary.light,
+              transition: "color 0.3s ease",
+            },
+          }}
+          href=""
+        >
+          <LinkedInIcon />
+        </IconButton>
+      </Stack>
+      <Typography
+        variant="body2"
+        sx={{
+          textAlign: "center",
+          marginTop: "1rem",
+          color: theme.palette.common.white,
+        }}
+      >
+        &copy; {new Date().getFullYear()} Taqatco. All rights reserved.
+      </Typography>
+    </Stack>
+  );
 };
 
 export default Footer;
